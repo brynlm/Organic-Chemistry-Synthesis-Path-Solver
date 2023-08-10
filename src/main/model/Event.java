25360607edb5285d7660e40783fa1010536c1d4c
brynleemeyer <brynleemeyer@gmail.com>
2023-08-10 14:38:24 -0700

    Forgot not supposed to edit the two classes; re-imported exact copies of the two classes

--- src/main/model/Event.java
+++ src/main/model/Event.java
@@ -1,6 +1,15 @@
 package model;
 
+import java.util.Calendar;
+import java.util.Date;
+
+
+/**
+ * Represents an alarm system event.
+ */
 public class Event {
+    private static final int HASH_CONSTANT = 13;
+    private Date dateLogged;
     private String description;
 
     /**
@@ -8,44 +17,52 @@ public class Event {
      * and the current date/time stamp.
      * @param description  a description of the event
      */
-
     public Event(String description) {
-
+        dateLogged = Calendar.getInstance().getTime();
         this.description = description;
     }
 
+    /**
+     * Gets the date of this event (includes time).
+     * @return  the date of the event
+     */
+    public Date getDate() {
+        return dateLogged;
+    }
 
     /**
      * Gets the description of this event.
      * @return  the description of the event
-     **/
-    public String getDescription () {
+     */
+
+    public String getDescription() {
         return description;
     }
 
-//    @Override
-//    public boolean equals(Object other) {
-//        if (other == null) {
-//            return false;
-//        }
-//
-//        if (other.getClass() != this.getClass()) {
-//            return false;
-//        }
-//
-//        Event otherEvent = (Event) other;
-//
-//        return (this.dateLogged.equals(otherEvent.dateLogged) &&
-//                this.description.equals(otherEvent.description));
-//    }
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) {
+            return false;
+        }
+
+        if (other.getClass() != this.getClass()) {
+            return false;
+        }
+
+        Event otherEvent = (Event) other;
+
+        return (this.dateLogged.equals(otherEvent.dateLogged)
+                &&
+                this.description.equals(otherEvent.description));
+    }
 
     @Override
     public int hashCode() {
-        return 0; //stub
+        return (HASH_CONSTANT * dateLogged.hashCode() + description.hashCode());
     }
 
     @Override
     public String toString() {
-        return ""; //stub
+        return dateLogged.toString() + "\n" + description;
     }
 }
